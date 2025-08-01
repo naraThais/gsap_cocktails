2df07450f79c5dbdfa440b096c8792f09c7089f2
Your Name <thaisna46@gmail.com>
2025-08-01 19:43:28 -0300

    Adicionando Layout (corpo do meu hero.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero"
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
